1cfedb0a103a48d2fe01c1e2e8316099f86991dc
Jesus_hna <thehacha167@gmail.com>
2023-11-28 16:29:01 +0100

    creacion proyecto y archivos

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# system_info.sh
- 
+
+# System Information Shell Script
+
+This shell script provides a simple menu to access various system information. The script includes the following options:
+
+1. **Disk Space**: This function (`disk_space`) shows the free disk space in human-readable format.
+2. **Directory Size**: This function (`dir_size`) prompts the user to enter a directory path and then displays the size of the specified directory.
+3. **CPU Usage**: This function (`cpu_usage`) displays the current CPU load.
+4. **Users Connected**: This function (`users_connected`) displays the number of users currently connected to the system.
+5. **Users Connected Since Last**: This function (`users_connected_since_last`) displays the number of users connected since the last time this function was called.
+
+The script runs in a loop, displaying a menu for the user to select an option. The `last_users` variable is initialized before the loop starts, storing the number of users connected at that time.
+
+To run the script, you can use the following command in a terminal:
+
+```shell
+bash system_info.sh
---
+++ system_info.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Función para obtener el espacio libre del disco
+function disk_space {
+    df -h
+}
+
+# Función para obtener el tamaño ocupado por un directorio
+function dir_size {
+    echo "Introduce la ruta del directorio:"
+    read dir
+    du -sh $dir
+}
+
+# Función para obtener el uso del procesador
+function cpu_usage {
+    top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+}
+
+# Función para obtener el número de usuarios conectados
+function users_connected {
+    who | wc -l
+}
+
+# Función para obtener el número de usuarios conectados desde la última vez que se preguntó
+function users_connected_since_last {
+    echo "Número de usuarios conectados desde la última vez: $last_users"
+    last_users=$(who | wc -l)
+}
+
+# Inicialización de la variable para usuarios conectados desde la última vez
+last_users=$(who | wc -l)
+
+# Bucle para mostrar el menú
+while true; do
+    echo "Menú de información del sistema:"
+    echo "1. Espacio libre del disco"
+    echo "2. Tamaño ocupado por un directorio"
+    echo "3. Uso del procesador"
+    echo "4. Número de usuarios conectados"
+    echo "5. Número de usuarios conectados desde la última vez que se preguntó"
+    echo "6. Salir"
+    echo "Elige una opción:"
+    read option
+
+    case $option in
+        1) disk_space ;;
+        2) dir_size ;;
+        3) cpu_usage ;;
+        4) users_connected ;;
+        5) users_connected_since_last ;;
+        6) break ;;
+        *) echo "Opción no válida";;
+    esac
+done
